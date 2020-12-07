cbab857f5c5a85dba99baadd56c67e05d9ad5751
benjamin-benoit <benjaminbenoitfr@gmail.com>
2020-12-07 18:09:10 +0100

    core data updates

---
+++ myTasks/myTasks/Models/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  myTasks
+//
+//  Created by Benjamin Benoit on 07/12/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
---
+++ myTasks/myTasks/Models/Task+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Task+CoreDataProperties.swift
+//  myTasks
+//
+//  Created by Benjamin Benoit on 07/12/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var title: Date?
+    @NSManaged public var uuid: UUID?
+
+}
+
+extension Task : Identifiable {
+
+}
